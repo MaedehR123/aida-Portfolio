43f413b312fbdf6edb6f40662e7c205faefc3026
maedeh <rezaei.maedeh2323@gmail.com>
2025-11-12 10:30:27 +0330

    Done Pre.js

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,13 @@
+// وارد کردن کتابخانه React برای ساخت کامپوننت
 import React from "react";
+
+// تعریف کامپوننت Pre که وظیفه نمایش یا عدم نمایش لودر (صفحه بارگذاری) را دارد
 function Pre(props) {
+  // بررسی مقدار props.load:
+  // اگر true باشد، المنت با id="preloader" نمایش داده می‌شود
+  // اگر false باشد، المنت با id="preloader-none" (یعنی پنهان) نمایش داده می‌شود
   return <div id={props.load ? "preloader" : "preloader-none"}></div>;
 }
 
+// خروجی کامپوننت برای استفاده در سایر بخش‌های پروژه
 export default Pre;
