04b6fe91bc608700c826f3172eaf540e62373674
maedeh <rezaei.maedeh2323@gmail.com>
2025-11-12 08:54:26 +0330

    atart file Navbar.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,6 @@ import {
 import { FaLinkedinIn, FaTelegramPlane } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
