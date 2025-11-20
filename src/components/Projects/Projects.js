5669e8a40cc1546c30aad1706afccd93630cef8c
maedeh <rezaei.maedeh2323@gmail.com>
2025-11-20 09:06:22 +0330

    Done

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -84,9 +84,8 @@ function Projects() {
             <ProjectCard
               imgPath={TravelList}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              title="چک لیست هوشمند سفر"
+              description="اپ تحت وب که باعث میشه بدون استرس و نگرانی وسایل خود را برای سفر اماده کنی و چک کنی که وسایل مورد نیاز خود را برداشتید"
              demoLink="https://blogs.soumya-jit.tech/"      
             />
           </Col>
