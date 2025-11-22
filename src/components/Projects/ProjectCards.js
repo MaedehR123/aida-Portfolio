dd9b32ef27a44ed4d138440800bca6fac0062fc5
maedeh <rezaei.maedeh2323@gmail.com>
2025-11-22 09:20:59 +0330

    add commit

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -5,22 +5,31 @@ import { CgWebsite } from "react-icons/cg";
 
 function ProjectCards(props) {
   return (
+    // 🔹 کامپوننت کارت پروژه – یک کارت شامل تصویر، عنوان، توضیحات و لینک دمو
     <Card className="project-card-view">
+      
+      {/* 🔹 نمایش تصویر پروژه در بالای کارت */}
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+
       <Card.Body>
+        
+        {/* 🔹 عنوان پروژه که از props دریافت می‌شود */}
         <Card.Title>{props.title}</Card.Title>
+
+        {/* 🔹 متن توضیحات پروژه، با استایل وسط‌چین و فاصله بالا */}
         <Card.Text style={{ textAlign: "center", marginTop:"25px"}}>
           {props.description}
         </Card.Text>
 
-        {/* 🔹 فقط دکمه مشاهده پروژه نمایش داده می‌شود */}
+        {/* 🔹 اگر لینک دمو ارسال شده باشد، دکمه "مشاهده پروژه" را نمایش بده */}
         {props.demoLink && (
           <Button
             variant="primary"
             href={props.demoLink}
-            target="_blank"
+            target="_blank"   // 🔹 باز شدن لینک در تب جدید
             style={{ marginTop: "10px" }}
           >
+            {/* 🔹 آیکون وبسایت + متن دکمه */}
             <CgWebsite /> &nbsp; {"مشاهده پروژه"}
           </Button>
         )}
