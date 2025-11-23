1fa43fab2df6374cde6e09aa41826550bc189b7e
maedeh <rezaei.maedeh2323@gmail.com>
2025-11-23 10:35:41 +0330

    change words

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -97,7 +97,7 @@ function NavBar() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <ImBlog style={{ marginBottom: "2px" }} /> پورتفولیو
+                <ImBlog style={{ marginBottom: "2px" }} /> پورتفولیو بیشتر
               </Nav.Link>
             </Nav.Item>
 
