baebddce63193cede95b1fb1eb97680b572fda8a
maedeh <rezaei.maedeh2323@gmail.com>
2025-11-23 11:12:14 +0330

    Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ function Home() {
             {/* ستون متن سمت راست */}
             <Col md={7} className="home-header" style={{ textAlign: "center" }}>
               {/* عنوان خوش آمدید */}
-              <h1 style={{ paddingBottom: 15 }} className="heading">
+              <h1 style={{ paddingBottom:15}} className="heading">
                 خوش آمدید{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
